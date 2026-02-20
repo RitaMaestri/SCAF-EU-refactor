working but wrong results.

src/simulation.py
@@ -25,7 +25,7 @@ warnings.filterwarnings("ignore")
 
 now = datetime.now()
 dt_string = now.strftime("%d-%m-%Y_%H:%M")
-exogenous_data = "REMIND_exogenous_data_2020"
+exogenous_data = "REMIND_exogenous_data_sectors"
 ###### PARAMETERS SETTING ###########
 
 # closure : "johansen" , "neoclassic", "kaldorian", "keynes-marshall", "keynes", "keynes-kaldor","neokeynesian1", "neokeynesian2"   ########
@@ -41,7 +41,7 @@ add_string = "REMIND-" + str(N) + "sectors"
 growth_ratios_df = pd.read_csv(
     "data/"+exogenous_data+".csv", index_col="variable") 
 
-years = np.array([eval(i) for i in growth_ratios_df.columns])
+years = np.array([eval(i) for i in growth_ratios_df.columns[3:]])
 stop = years[-1]
 start = years[0]
 
@@ -82,7 +82,7 @@ growth_ratios=build_exogenous_timeseries(sectors, exo_mask, growth_ratios_df, [s
                               sector2_col="Sector_2")
 
 System=sys_df(years, growth_ratios, variables_calibration, parameters_calibration)
-
+System.parameters_df.to_csv("results/parameters_df_data_sectors.csv")
 
 ####### check for errors ########
 

src/time_series_data.py
@@ -24,6 +24,8 @@ class sys_df:
 
 
         for i in self.growth_ratios.keys():
+            if i=="aYij":
+                a=1
             par_t0=self.calib_par_dict[i] 
             gr_j=self.growth_ratios[i]
 
@@ -68,7 +70,7 @@ class sys_df:
 
         for key in dynamic_par:
             if isinstance(self.calib_par_dict[key], np.ndarray):
-                exo_index = ~np.isnan(self.calib_par_dict[key])#where there are not nans it means it is exogenous. Non nans are
+                exo_index = ~np.isnan(self.calib_par_dict[key]).flatten()#where there are not nans it means it is exogenous. Non nans are
                 
                 new_df_slice=self.parameters_df.loc[key]
                 