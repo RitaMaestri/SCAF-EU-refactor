0ea6c646232e33bdac8c60a4a67cd9e3295b776a
RitaMaestri <rita.maestri95@gmail.com>
2026-04-17 18:41:30 +0200

    results_2026-04-17_18-40

--- Data_postprocessing/run.py
+++ Data_postprocessing/run.py
@@ -15,7 +15,7 @@ from lib import extract_var_df, plot_varj_evol, plot_varj_evol_absolute, plot_va
 
 no_SC_path = "Solver/results/tagged/results_2026-03-30_19-03/results_2026-03-30_19-03.csv"
 # Set to a specific CSV path to plot a single run, or None to plot all tagged results
-results_path = "Solver/results/drafts/results_2026-04-17_17-44/results_2026-04-17_17-44.csv"
+results_path = "Solver/results/drafts/results_2026-04-17_17-54/results_2026-04-17_17-54.csv"
 #results_path = None
 
 # Load shared data once
